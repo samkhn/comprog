d00aa7b22164e44412dc8db5f291bfe56296f289
Samiur Khan <samiurkh1n@gmail.com>
2023-02-28 21:54:46 -0500

    A bunch o' dynamic programming. Cool technique. Weird name

---
+++ src/techniques/dp/01knapsack.cpp
@@ -0,0 +1,51 @@
+// 01 Knapsack
+// 0/1 := cannot break choice down
+// Given a set of objects, each w/ a value and a weight, what is the maximum
+// value we can obtain by selecting a subset s.t. the sum of weights does not
+// exceed some [knapsack] capacity.
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// In each pair, first is value, second is weight.
+using Knapsack = std::vector<std::pair<int, int>>;
+
+int MaxKnapsackWeight(const Knapsack &os, int capacity) {
+  int object_count = os.size();
+  std::vector<std::vector<int>> m(object_count + 1,
+                                  std::vector<int>(capacity + 1, 0));
+  for (int i = 1; i <= object_count; ++i) {
+    for (int j = 0; j <= capacity; ++j) {
+      if (os[i-1].second > j) {
+        m[i][j] = m[i-1][j];
+      } else {
+        m[i][j] = std::max(m[i-1][j], os[i-1].first+m[i-1][j-os[i-1].second]);
+      }
+    }
+  }
+  return m[object_count][capacity];
+}
+
+struct TestCase {
+  std::string name;
+  Knapsack in_objects;
+  int in_capacity;
+  int want;
+};
+
+int main() {
+  std::vector<TestCase> tests = {
+      {"Ex1", {}, 1, 0},
+      {"Ex2", {{2, 3}, {2, 1}, {4, 3}, {5, 4}, {3, 2}}, 7, 10},
+  };
+  for (auto t : tests) {
+    if (MaxKnapsackWeight(t.in_objects, t.in_capacity) != t.want) {
+      std::cout << "FAIL " << t.name << "\n";
+    } else {
+      std::cout << "PASS " << t.name << "\n";
+    }
+  }
+  return 0;
+}
---
+++ src/techniques/dp/staircase.cpp
@@ -0,0 +1,50 @@
+// Staircase
+// LeetCode 70
+// You are climbing a stair case. It takes n steps to reach to the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways
+// can you climb to the top?
+// Note: Given n will be a positive integer.
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int countWaysToClimb(std::vector<int> &m, int n) {
+  if (n >= 0 && n <= 2) {
+    return n;
+  }
+  if (m[n] == 0) {
+    int takeOne = countWaysToClimb(m, n-1);
+    int takeTwo = countWaysToClimb(m, n-2);
+    m[n] = takeOne + takeTwo;
+  }
+  return m[n];
+}
+
+int CountWaysToClimbStairs(int n) {
+  std::vector<int> m(n+1, 0);
+  return countWaysToClimb(m, n);
+}
+
+struct TestCase {
+  std::string name;
+  int in;
+  int want;
+};
+
+int main() {
+  std::vector<TestCase> tests = {
+    {"Ex1", 2, 2},
+    {"Ex2", 3, 3},
+  };
+
+  for (auto t : tests) {
+    if (CountWaysToClimbStairs(t.in) != t.want) {
+      std::cout << "FAIL " << t.name << "\n";
+    } else {
+      std::cout << "PASS " << t.name << "\n";
+    }
+  }
+  return 0;
+}
